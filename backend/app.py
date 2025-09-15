c6ac2a5a47116740b171476f044190d4d24ca3d8
shaqib1911 <shaqibshaikh1911@gmail.com>
2025-09-15 13:00:56 +0530

    changes in the backend

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Welcome to the Simple Flask Backend testing for jenkins"
+        "message": "Welcome to the Simple Flask Backend"
     })
 
 if __name__ == '__main__':
